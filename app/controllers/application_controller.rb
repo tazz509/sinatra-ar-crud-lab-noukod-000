dba7c120b37fa5ea4ae30d111d777a80c7f27c11
carly <carly@noukod.com>
2019-01-16 16:01:10 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -11,4 +10,48 @@ class ApplicationController < Sinatra::Base
   get '/' do
 
   end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    Post.create(name:@params[:name],content:@params[:content])
+    @posts=Post.all
+    erb :index
+  end
+
+  get '/posts' do
+    @posts=Post.all
+    erb :index
+  end
+
+  get '/posts/:id/edit' do
+    @post_id=@params[:id]
+    @post=Post.find_by({id:@params[:id]})
+    erb :edit
+  end
+
+  get '/posts/:id' do
+    @post=Post.find_by({id:@params[:id]})
+    erb :show
+  end
+
+  patch '/posts/:id' do
+    @id=@params[:id]
+    @post=Post.find_by({id:@id})
+    @post.name=@params[:name]
+    @post.content=@params[:content]
+    @post.save
+    redirect('/posts/'+@id)
+  end
+
+  delete '/posts/:id' do
+    @id=@params[:id]
+    @post=Post.find_by({id:@id})
+    @post.destroy
+  end
+
+
+
 end
